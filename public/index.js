86327f4586e337a5fd1f2501bfa63500e47b5630
Guillermo San Martín <guille.seba0802@gmail.com>
2023-04-26 20:44:29 -0300

    form styles

--- public/index.js
+++ public/index.js
@@ -1,3 +1,18 @@
-const form = document.getElementById('form');
+const inputs = document.querySelectorAll('.input');
 
-form.addEventListener('submit', e => {});
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add('focus');
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == '') {
+    parent.classList.remove('focus');
+  }
+}
+
+inputs.forEach(input => {
+  input.addEventListener('focus', focusFunc);
+  input.addEventListener('blur', blurFunc);
+});
