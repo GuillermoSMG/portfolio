855f646894ca7115988e8af4c32534aa8d1f3d66
Guillermo San Martín <guille.seba0802@gmail.com>
2023-09-02 15:19:18 -0300

    updated

--- public/index.js
+++ public/index.js
@@ -84,3 +84,25 @@ function sendedMail() {
     },
   }).showToast();
 }
+/* document.addEventListener('mousemove', event => {
+  document.querySelectorAll('.layer').forEach(layer => {
+    const speed = layer.getAttribute('data-speed');
+    const x = (window.innerWidth - event.pageX * speed) / 70;
+
+    const y = (window.innerHeight - event.pageY * speed) / 70;
+
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+});
+ */
+
+document.addEventListener('mousemove', event => {
+  document.querySelectorAll('.layer').forEach(layer => {
+    const speed = layer.getAttribute('data-speed');
+    const x = (window.innerWidth - event.pageX * speed) / 100;
+
+    const y = (window.innerHeight - event.pageY * speed) / 100;
+    console.log(x, y);
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+});
